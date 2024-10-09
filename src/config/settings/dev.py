57a0ffe1abcc2963357dc2f30ecc35568cd594ce
joisemp <joise1124@gmail.com>
2024-10-09 16:05:34 +0530

    Getting ready for test production

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -6,6 +6,18 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
